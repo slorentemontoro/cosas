2c1f386af1bbca6fed5d41e81b128efa8d57c32f
Sergio Lorente <slorentemontoro@gmail.com>
2022-04-29 16:25:56 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { BannersComponent } from './banners/banners.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CarouselComponent,
+    BannersComponent,
+    CategoriasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [NavBarComponent]
 })
 export class AppModule { }
